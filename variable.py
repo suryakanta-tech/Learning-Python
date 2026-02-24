094d309db455411069f16b5b13f5d5652dc734ac
suryakantapradhannn-coder <253405826+suryakantapradhannn-coder@users.noreply.github.com>
2026-02-24 21:07:57 +0530

    Python variable

---
+++ variable.py
@@ -0,0 +1,52 @@
+# VARIABLE LEARNING SECTION
+
+number = 20 # number = variable
+            # 20 = value of the number variable
+print(number)  
+
+
+# Creating Variables
+
+#dynamic type variable
+name = "Surya"
+age = 20
+mark = 69.5
+
+print(name)
+print(age)
+print(mark)
+
+
+# Variable Naming Rules: # 1.start with a letter or an underscore.
+                         # 2.cannot start with a number.
+                         # 3.no space
+                         # 4.Case-sensitive (age ≠ Age)
+                         # 5.no special character.
+
+my_name = "Surya" 
+age2 = 20
+_total = 100
+
+
+# Multiple Variables
+
+# one line many variables
+a,b,c = 10,20,30
+print(a,b,c)
+# same value
+a = b = c = 10
+print(a,b,c)
+
+
+# Variables Store Different Data Types
+
+age = 20 # integer
+name = "Surya" # string
+mark = 89.5 # float
+is_student = True # boolean 
+
+print(type(age)) # <class 'int'>
+print(type(name)) # <class 'str'>
+print(type(mark)) # <class 'float'>
+print(type(is_student)) # <class 'bool'>
+

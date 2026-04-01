e269b90c9cad6c040eea9405cc2cfa2cfd4100ee
suryakanta-tech <253405826+suryakanta-tech@users.noreply.github>
2026-04-01 15:01:31 +0530

    REPL in Python

--- create_file.py
+++ create_file.py
@@ -1,7 +1,10 @@
-# f = open('print form.txt', 'x')
+'''f = open('print form.txt', 'x')
+f.close()'''
 
-# f.close()
+'''f = open('git commands.txt', 'x')
 
-f = open('git commands.txt', 'x')
+f.close()'''
+
+f = open('REPL in python.txt', 'x')
 
 f.close()

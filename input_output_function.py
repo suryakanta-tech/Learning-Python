2c443375d5720ec81ffc1c77ba4538e6ec40add1
suryakanta-tech <253405826+suryakanta-tech@users.noreply.github>
2026-04-09 06:41:18 +0530

    Python input output function

---
+++ input_output_function.py
@@ -0,0 +1,32 @@
+# output function
+
+# example.1
+print("Learning python is fun and I enjoy it.")
+
+# example.2
+a = 2
+print("The value of a is ",a)
+
+''' syntax of print function
+-------------------------------
+print(*objects, sep=' ', end='/n', file = sys.stdout, flush = False) '''
+
+# example
+print(1,2,3,4)
+print(1,2,3,4,sep = '+')
+print(1,2,3,4,sep = '+',end = '%')
+
+
+# input function
+
+# example.1
+name = input("Enter your name: ")
+print("Your name is ",name)
+
+# example.2
+n = input("Enter a number: ")
+print("The number is ",n)
+
+# example.3
+n = input("Enter an integer ")
+print("The integer is ",n)

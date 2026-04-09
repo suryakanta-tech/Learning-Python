eefeae5e4fab20f2372b3baaa680f887dc129054
suryakanta-tech <253405826+suryakanta-tech@users.noreply.github>
2026-04-09 06:42:51 +0530

    Python Indentation

---
+++ indentation.py
@@ -0,0 +1,29 @@
+# indentation in python 
+
+if True:
+  print("Correct")
+else:
+  print("Wrong")
+# =============================
+
+if True:
+  print("Answer")
+  print("Correct")
+else:
+  print("Wrong")
+  print("Answer")
+
+# multiple statement
+if True:
+  print("Correct")
+elif False:
+  print("Wrong")
+else:
+  print("Invalid statement!")
+
+
+# quptes in python
+word = "Single word."
+sentence = "This is a sentence."
+paragraph = """ This a logn paragraph. It consists of several
+                lines and sentence. """

a4053543218e68f9b0d40af878f63cbe58c60aaf
suryakanta-tech <253405826+suryakanta-tech@users.noreply.github>
2026-04-09 06:38:53 +0530

    Python Variable

---
+++ variable.py
@@ -0,0 +1,27 @@
+# variable
+
+a = 100  # an integer assignment
+b = 100.0  # a floating point
+name = "Surya"  # a string
+
+# a,b,c are the variables
+# 100,100.0,"Surya" are the values
+
+print(a)
+print(b)
+print(name)
+# ======================================
+
+# assign a single value to several variables simultaneously
+a = b = c = 100
+print(a)
+print(b)
+print(c)
+# =======================================
+
+# assign multiple object to multiple variables simultaneously
+a,b,c = 1,2,"Surya"
+
+print(a)
+print(b)
+print(c)
